87fc16ec548a92042c64cb950ce9787f2179afec
heyichingg <heyizhongg@gmail.com>
2024-01-24 22:59:16 +0800

    Add Player

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rust_invaders::{frame::{self, new_frame}, render};
+use rust_invaders::{frame::{self, new_frame, Drawable}, render, player::Player};
 use rusty_audio::Audio;
 use std::{error::Error, time::Duration, sync::mpsc, {io,thread}};
 
@@ -37,14 +37,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     //Game Loop
+    let mut player = Player::new();
     'gameloop: loop {
         // Per-frame init 
-        let curr_frame = new_frame();
+        let mut curr_frame = new_frame();
 
         // Input 
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()?{
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -55,6 +58,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
 
         // Draw & render
+        player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
